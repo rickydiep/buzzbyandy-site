be2e30f90938c93ab3935fe9bba5e52d68121a32
rickydiep <35702442+rickydiep@users.noreply.github.com>
2018-11-13 13:42:50 -0600

    fixed bio section

--- file.js
+++ file.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     var feed = new Instafeed ({
         get:"user",
         userId:"1531587193",
-        limit: 12,
+        limit: 8,
         sortBy: "most-recent",
         resolution: "standard_resolution",
         accessToken: "1531587193.1677ed0.a24077c78de34f6a9fcd7538c6fb6c98",
-        template: '<div class="image-box col-lg-3 py-3"><a href="{{link}}" target="_blank"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); filter:grayscale(100%); width="100%"></div></a></div>'
+        template: '<li class="image-box col-lg-3 col-md-4 col-6 py-3"><a href="{{link}}" target="_blank"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); filter:grayscale(100%); width="100%"></div></a></li>'
 });
     feed.run(); 
 
