6ef7f20fc573321640da97f1ec1bf6a2e8d41a37
rickydiep <35702442+rickydiep@users.noreply.github.com>
2018-11-30 15:36:16 -0600

    adding fullpage

--- file.js
+++ file.js
@@ -6,19 +6,34 @@ $(document).ready(function(){
         sortBy: "most-recent",
         resolution: "standard_resolution",
         accessToken: "1531587193.1677ed0.a24077c78de34f6a9fcd7538c6fb6c98",
-        template: '<li class="image-box col-lg-3 col-md-4 col-6 py-3"><a href="{{link}}" target="_blank"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); width="100%"></div></a></li>'
+        template: '<li class="image-box col-lg-3 col-md-4 col-6 py-3"><a href="{{link}}" target="_blank"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); filter:greyscale(100); width="100%"></div></a></li>'
 });
-    feed.run(); 
+    feed.run();
 
+    
     $('.nav-link').on('click', function(){
         $('#navbarNav').removeClass('show');
     });
 
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-    }
-    
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-    }
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('#sidebar').removeClass('active');
+        // hide overlay
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+
 });
